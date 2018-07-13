594d565f04f3ae32befbd47385bc102541680a04
admin <admin@DESKTOP-BH7UL17>
2018-07-13 18:21:31 +0200

    Created Even and person Entities

---
+++ friendsnet/src/main/java/com/everis/alicante/courses/beca/java/friendsnet/persistence/entity/Person.java
@@ -0,0 +1,20 @@
+package com.everis.alicante.courses.beca.java.friendsnet.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String surname;
+
+}
